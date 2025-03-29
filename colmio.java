c0ba14447a73595da35016175d9a41ce11abb624
cri21gut <cris21gutierrez@gmail.com>
2025-03-28 22:46:37 -0600

    tarea programacion 1

--- colmio.java
+++ colmio.java
@@ -1,19 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+import java.util.Scanner;
 
-/**
- *
- * @author cris2
- */
-public class colmio {
-
-    /**
-     * @param args the command line arguments
-     */
+public class NumeroVampiro {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el límite inferior del rango:");
+        int limiteInferior = scanner.nextInt();
+        System.out.println("Ingrese el límite superior del rango:");
+        int limiteSuperior = scanner.nextInt();
+
+        int numeroVampiro = encontrarNumeroVampiro(limiteInferior, limiteSuperior);
+        if (numeroVampiro != -1) {
+            System.out.println("El primer número vampiro dentro del rango es: " + numeroVampiro);
+        } else {
+            System.out.println("No se encontró ningún número vampiro dentro del rango.");
+        }
     }
-    
+
+    public static int encontrarNumeroVampiro(int limiteInferior, int limiteSuperior) {
+        int numeroVampiro = -1;
+        do {
+            limiteInferior++;
+            if (esNumeroVampiro(limiteInferior)) {
+                numeroVampiro = limiteInferior;
+                break;
+            }
+        } while (limiteInferior <= limiteSuperior);
+        return numeroVampiro;
+    }
+
+    public static boolean esNumeroVampiro(int numero) {
+        int longitud = String.valueOf(numero).length();
+        if (longitud % 2 != 0) {
+            return false;
+        }
+        int mitadLongitud = longitud / 2;
+        for (int i = (int) Math.pow(10, mitadLongitud - 1); i < (int) Math.pow(10, mitadLongitud); i++) {
+            int posibleColmillo2 = numero / i;
+            if (posibleColmillo2 * i == numero && esColmilloValido(i, posibleColmillo2, numero)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean esColmilloValido(int colmillo1, int colmillo2, int numero) {
+        String concatenacion = String.valueOf(colmillo1) + String.valueOf(colmillo2);
+        String numeroCadena = String.valueOf(numero);
+        for (char c : numeroCadena.toCharArray()) {
+            if (!concatenacion.contains(String.valueOf(c))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- peraciones.java
+++ peraciones.java
@@ -1,19 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+import java.util.Scanner;
 
-/**
- *
- * @author cris2
- */
-public class peraciones {
-
-    /**
-     * @param args the command line arguments
-     */
+public class EcuacionCuadratica {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        double a, b, c;
+        
+        do {
+            System.out.print("Introduce el valor de a (no puede ser 0): ");
+            a = scanner.nextDouble();
+            if (a == 0) {
+                System.out.println("El valor de 'a' no puede ser cero en una ecuación cuadrática.");
+            }
+        } while (a == 0);
+      
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.print("Introduce el valor de b: ");
+            b = scanner.nextDouble();
+            System.out.print("Introduce el valor de c: ");
+            c = scanner.nextDouble();
+            validInput = true; 
+        }
+        
+        double discriminante = b * b - 4 * a * c;
+
+        for (int i = 0; i < 1; i++) {
+            if (discriminante > 0) {
+                // Dos soluciones reales distintas
+                double raiz1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+                double raiz2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+                System.out.println("Las soluciones reales son: ");
+                System.out.println("Raíz 1: " + raiz1);
+                System.out.println("Raíz 2: " + raiz2);
+                break; //
+            } else if (discriminante == 0) {
+            }
+                double raiz = -b / (2 * a);
+                System.out.println("La solución real es: " + raiz);
+                break;
+            } else {
+                System.out.println("No hay soluciones reales.");
+                break;
+            }
+        }
+        
+        for (int i = 0; i < 3; i++) {
+        
+        for (int i = 0; i < 3; i++) {
+            if (i == 1) {
+                continue; 
+            }
+            System.out.println("Iteración " + i);
+        }
     }
-    
 }
